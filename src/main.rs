ae58e13eb9173292a5dc62c012fd96850a6df7c5
Michael Ziegler <diese-addy@funzt-halt.net>
2017-11-26 19:30:16 +0100

    add --help and options; remove debug noise; refresh data periodically

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,14 @@
 extern crate snmp;
 #[macro_use]
 extern crate error_chain;
+extern crate clap;
 extern crate uname;
 extern crate libc;
 
 use std::collections::BTreeMap;
 use std::net::{UdpSocket,SocketAddr};
+use std::time::{Instant,Duration};
+use clap::{Arg, App};
 use snmp::SnmpPdu;
 use snmp::pdu;
 
@@ -33,44 +36,58 @@ mod mib_sys;
 mod mib_disks;
 mod mib_net;
 
-fn run(port: u32, community: &str) -> Result<()> {
+fn run(matches: clap::ArgMatches) -> Result<()> {
+    let port = matches.value_of("port").unwrap_or("161").parse::<u16>()
+        .chain_err(|| "Port argument must be a number between 1 and 65535")?;
+    let community = matches.value_of("community").unwrap_or("sunt");
+
     let addr: SocketAddr = format!("[::]:{}", port).parse()
         .chain_err(|| "Could not parse address")?;
 
     let socket = UdpSocket::bind(addr)
         .chain_err(|| "Could not bind to socket")?;
 
-    let mut values: BTreeMap<OID, Value> = BTreeMap::new();
+    socket.set_read_timeout(Some(Duration::new(1, 0)))
+        .chain_err(|| "could not set timeout")?;
 
-    mib_sys::get_system(&mut values, "1.3.6.1.2.1.1");
-    mib_disks::get_disks(&mut values, "1.3.6.1.4.1.2021.13.15.1.1");
-    mib_disks::get_filesystems(
-        &mut values,
-        "1.3.6.1.2.1.25.2.3.1",
-        "1.3.6.1.4.1.2021.9.1"
-    );
-    mib_net::get_interfaces(
-        &mut values,
-        "1.3.6.1.2.1.2.2.1",
-        "1.3.6.1.2.1.31.1.1.1"
-    );
+    let mut values: BTreeMap<OID, Value> = BTreeMap::new();
+    let mut last_refresh : Option<Instant> = None;
 
     let mut buf = [0 as u8; 16 * 1024];
     loop {
+        if last_refresh.is_none() ||
+           last_refresh.unwrap() + Duration::new(15, 0) < Instant::now() {
+            mib_sys::get_system(
+                &mut values,
+                "1.3.6.1.2.1.1"
+            );
+            mib_disks::get_disks(
+                &mut values,
+                "1.3.6.1.4.1.2021.13.15.1.1"
+            );
+            mib_disks::get_filesystems(
+                &mut values,
+                "1.3.6.1.2.1.25.2.3.1",
+                "1.3.6.1.4.1.2021.9.1"
+            );
+            mib_net::get_interfaces(
+                &mut values,
+                "1.3.6.1.2.1.2.2.1",
+                "1.3.6.1.2.1.31.1.1.1"
+            );
+            last_refresh = Some(Instant::now());
+        }
+
         if let Ok((data_len, client_addr)) = socket.recv_from(&mut buf) {
             let pdu_bytes = &buf[0..data_len];
 
-            println!("got {} dataz from client {}: {:?}", data_len, client_addr, pdu_bytes);
-
             if let Ok(req) = SnmpPdu::from_bytes(pdu_bytes) {
-                println!("request type: {:?}", req.message_type);
-                println!("req_id:        {}", req.req_id);
-
                 let mut start_from_oid = OID::from_parts(&["1"]);
 
                 for (name, _) in req.varbinds {
-                    start_from_oid = OID::from_object_identifier(name);
-                    println!("Client wants start OID {}", &start_from_oid);
+                    if name.to_string() != "Invalid OID: AsnInvalidLen" {
+                        start_from_oid = OID::from_object_identifier(name);
+                    }
 
                     // snmptable likes to query OIDs that don't exist as start OID.
                     // We catch that by checking if the OID actually exists, and if not,
@@ -80,7 +97,6 @@ fn run(port: u32, community: &str) -> Result<()> {
                         start_oid_vec.pop();
                         start_from_oid = OID::from_vec(&start_oid_vec);
                     }
-                    println!("Start OID is {}", &start_from_oid);
                     break;
                 }
 
@@ -106,7 +122,6 @@ fn respond (
 
     for (oid, val) in values {
         if !found_start && oid.is_subtree_of(&start_from_oid) {
-            println!("Found start OID {} => {}", start_from_oid, oid.str());
             found_start = true;
             if *oid == start_from_oid {
                 continue;
@@ -147,7 +162,23 @@ fn respond (
 
 
 fn main(){
-    if let Err(ref e) = run(1161, "sunt") {
+    let matches = App::new("sunt")
+        .version("0.0.1")
+        .author("Michael Ziegler <diese-addy@funzt-halt.net>")
+        .about("SNMP Agent for Linux")
+        .arg(Arg::with_name("port")
+            .short("p")
+            .long("port")
+            .takes_value(true)
+            .help("Port number to use [161]"))
+        .arg(Arg::with_name("community")
+            .short("c")
+            .long("community")
+            .takes_value(true)
+            .help("Community to use in responses [sunt]"))
+        .get_matches();
+
+    if let Err(ref e) = run(matches) {
         eprintln!("error: {}", e);
     }
 }
